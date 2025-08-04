e566dc0323c6f031ceee6a5f97b42188b6d27a1e
Mikhil M <mighilmurali@gmail.com>
2025-08-04 22:43:33 +0530

    Add cookie-parser and cors middleware; set up Express app with error handling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({ path: './.env' });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on('error', (err) => {
+        console.error("Express app encountered an error:", err);
+        throw err;
+    });
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    })
+})
+.catch((error) => {
+    console.error(" MongoDB Connection Failed:", error);
+    process.exit(1);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // One way to connect to MongoDB using Mongoose and Express - Not a recommented way as it is directly writeen in the index.js file
 /*
